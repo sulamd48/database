9ef68760a9a5b920070d7d4c4da6fee78285c69b
sulamd48 <sulamd48@gmail.com>
2026-07-17 22:40:03 +0530

    Create link.js

---
+++ link.js
@@ -0,0 +1,7 @@
+[
+  "https://sula-test-bots-e9e0add6ace7.herokuapp.com",
+  "https://sula-mini-app-appphhp-1-5da34a7a7598.herokuapp.com",
+  "https://sula-mini-app-appphhp-2-013b2c4f9221.herokuapp.com",
+  "https://sula-mini-app-appphhp-3-a9a54224ea19.herokuapp.com",
+  "https://sula-mini-app-appphhp-4-36c713c956a7.herokuapp.com"
+]
